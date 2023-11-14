dbdbd5b724e4dd4adc7795de84c9952d148d9e9a
Matthew Sellings <99349293+sgmselli@users.noreply.github.com>
2023-11-14 00:59:18 +0000

    changed title name

--- src/header/Header.jsx
+++ src/header/Header.jsx
@@ -40,48 +40,52 @@ function Header() {
     return (
         
             <Stack direction={{base:'column', md: 'row'}} width='100%' pl={{base:0, md:'4%'}} pr={{base:0, md:'10%'}} pt={{base:'4vh', md: '2%'}} position='absolute'  zIndex='10'  >
-                <Flex justifyContent={{base:'center', xl:'start'}}>
-                 <Heading  size='xl'onClick={(location) => navigateTo('')} cursor='pointer' >Natasha</Heading>
+                <Flex justifyContent={{base:'center', lg:'start'}}>
+                    <HStack spacing={0}>
+                    <Heading  size='lg'onClick={(location) => navigateTo('')} cursor='pointer' >Natasha</Heading>
+                    <Heading  size='lg'onClick={(location) => navigateTo('')} color='red.400' cursor='pointer' >Chugani</Heading>
+                    </HStack>
+                 
               
                 </Flex>
                 <Spacer />
                 
-                <HStack pt={{base:'4vh', md:'0'}} justifyContent={'center'} spacing={{base:'5', md: '16'}}>
+                <HStack pt={{base:'5vh', md:'0'}} justifyContent={'center'} spacing={{base:'5', md: '16'}}>
 
                     {page === '' ?
                         <VStack>
                             <CurrentPageBar width='58px' />
-                            <Text onClick={(location) => navigateTo('')}  cursor='pointer' color='red.400' fontSize='xl'>Home</Text> 
+                            <Text onClick={(location) => navigateTo('')}  cursor='pointer' color='red.400' fontSize='lg'>Home</Text> 
                         </VStack>
                     :
-                        <Text onClick={(location) => navigateTo('')} _hover={{color: 'red.400'}} cursor='pointer' fontSize='xl'>Home</Text> 
+                        <Text onClick={(location) => navigateTo('')} _hover={{color: 'red.400'}} cursor='pointer' fontSize='lg'>Home</Text> 
                     }
                     
                     {page === 'about' || aboutHover ?
                         <VStack>
                             <CurrentPageBar width='58px' />
-                            <Text fontSize='xl' onClick={(location) => navigateTo('about')} color='red.400' cursor='pointer'>About</Text>
+                            <Text fontSize='lg' onClick={(location) => navigateTo('about')} color='red.400' cursor='pointer'>About</Text>
                         </VStack>
                     :
-                        <Text fontSize='xl' onClick={(location) => navigateTo('about')} _hover={{color: 'red.400'}} cursor='pointer'>About</Text>
+                        <Text fontSize='lg' onClick={(location) => navigateTo('about')} _hover={{color: 'red.400'}} cursor='pointer'>About</Text>
                     }
                     
                     {page === 'work' || workHover ?
                         <VStack>
                             <CurrentPageBar width='50px' />
-                            <Text fontSize='xl' onClick={(location) => navigateTo('work')}  color='red.400' cursor='pointer'>Work</Text>
+                            <Text fontSize='lg' onClick={(location) => navigateTo('work')}  color='red.400' cursor='pointer'>Work</Text>
                         </VStack>
                     :
-                        <Text fontSize='xl' onClick={(location) => navigateTo('work')} _hover={{color: 'red.400'}} cursor='pointer'>Work</Text>
+                        <Text fontSize='lg' onClick={(location) => navigateTo('work')} _hover={{color: 'red.400'}} cursor='pointer'>Work</Text>
                     }
 
                     {page === 'contact' || contactHover ?
                         <VStack>
                             <CurrentPageBar width='70px' />
-                            <Text fontSize='xl' onClick={(location) => navigateTo('contact')}  color='red.400' cursor='pointer'>Contact</Text>
+                            <Text fontSize='lg' onClick={(location) => navigateTo('contact')}  color='red.400' cursor='pointer'>Contact</Text>
                         </VStack>
                     :
-                        <Text fontSize='xl' onClick={(location) => navigateTo('contact')} _hover={{color: 'red.400'}} cursor='pointer'>Contact</Text>
+                        <Text fontSize='lg' onClick={(location) => navigateTo('contact')} _hover={{color: 'red.400'}} cursor='pointer'>Contact</Text>
                     }
                     
                 </HStack>
